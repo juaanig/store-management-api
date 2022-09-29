bc2a6582fef9742a8ae7036ec12f1499e3d514fc
juani <juangarnero@hotmail.com.ar>
2022-09-29 09:22:28 -0300

    fist commit sm-api

---
+++ store-management-api/Entities/Producto.cs
@@ -0,0 +1,22 @@
+﻿namespace store_management_api.Entities
+{
+    public class Producto
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public int quantity { get; set; }
+        public decimal price { get; set; }
+        public DateTime expDate { get; set; }
+        public DateTime entryDate { get; set; }
+
+        public void generatorID() 
+        {
+            Random r = new Random();
+            this.id = this.name + "000" + r.Next(100,1001);
+        }
+
+
+
+
+    }
+}
---
+++ store-management-api/Entities/Usuarios.cs
@@ -0,0 +1,18 @@
+﻿namespace store_management_api.Entities
+{
+    public class Usuarios
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private string lastName { get; set; }
+        private string mail { get; set; }
+        private string password { get; set; }
+        public string role { get; set; }        
+
+    }
+
+    // clase usuarios con distintos roles 
+    // clase producto 
+    // clase ubicacion 
+
+}
